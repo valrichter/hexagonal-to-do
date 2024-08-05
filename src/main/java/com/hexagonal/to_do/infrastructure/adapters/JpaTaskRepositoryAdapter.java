78d941ffc4769d359ca7c0822d3c1491ff46b069
valrichter <valenrichter11@gmail.com>
2024-08-05 17:04:42 -0300

    Add implemetations of JpaTaskRepositoryAdapter

--- src/main/java/com/hexagonal/to_do/infrastructure/adapters/JpaTaskRepositoryAdapter.java
+++ src/main/java/com/hexagonal/to_do/infrastructure/adapters/JpaTaskRepositoryAdapter.java
@@ -1,4 +1,56 @@
 package com.hexagonal.to_do.infrastructure.adapters;
 
-public interface JpaTaskRepositoryAdapter {
+import com.hexagonal.to_do.domain.models.Task;
+import com.hexagonal.to_do.domain.ports.out.TaskRepositoryPort;
+import com.hexagonal.to_do.infrastructure.entities.TaskEntity;
+import com.hexagonal.to_do.infrastructure.repositories.JpaTaskRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class JpaTaskRepositoryAdapter implements TaskRepositoryPort {
+
+    private final JpaTaskRepository jpaTaskRepository;
+
+    public JpaTaskRepositoryAdapter(JpaTaskRepository jpaTaskRepository) {
+        this.jpaTaskRepository = jpaTaskRepository;
+    }
+
+    @Override
+    public Task save(Task task) {
+        TaskEntity taskEntity = TaskEntity.fromDomainModel(task);
+        TaskEntity savedTaskEntity = jpaTaskRepository.save(taskEntity);
+        // TODO: Mapper
+        return savedTaskEntity.toDomainModel();
+    }
+
+    @Override
+    public Optional<Task> findById(Long id) {
+        return jpaTaskRepository.findById(id).map(TaskEntity::toDomainModel);
+    }
+
+    @Override
+    public List<Task> findAll() {
+        return jpaTaskRepository.findAll().stream().map(TaskEntity::toDomainModel).toList();
+    }
+
+    @Override
+    public Optional<Task> update(Task task) {
+        if(jpaTaskRepository.existsById(task.getId())) {
+            TaskEntity taskEntity = TaskEntity.fromDomainModel(task);
+            TaskEntity savedTaskEntity = jpaTaskRepository.save(taskEntity);
+            // TODO: Mapper
+            return Optional.of(savedTaskEntity.toDomainModel());
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        if(jpaTaskRepository.existsById(id)) {
+            jpaTaskRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/com/hexagonal/to_do/infrastructure/repositories/JpaTaskRepository.java
+++ src/main/java/com/hexagonal/to_do/infrastructure/repositories/JpaTaskRepository.java
@@ -1,4 +1,7 @@
 package com.hexagonal.to_do.infrastructure.repositories;
 
-public interface JpaTaskRepository {
+import com.hexagonal.to_do.infrastructure.entities.TaskEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaTaskRepository extends JpaRepository<TaskEntity, Long> {
 }
