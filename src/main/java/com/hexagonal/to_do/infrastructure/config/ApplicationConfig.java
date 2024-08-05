b64e80b3be428ac0d1727a879da4d66cf1277980
valrichter <valenrichter11@gmail.com>
2024-08-05 17:19:36 -0300

    Add implementation of ApplicationConfig

--- src/main/java/com/hexagonal/to_do/infrastructure/config/ApplicationConfig.java
+++ src/main/java/com/hexagonal/to_do/infrastructure/config/ApplicationConfig.java
@@ -1,4 +1,44 @@
 package com.hexagonal.to_do.infrastructure.config;
 
+import com.hexagonal.to_do.application.usecases.*;
+import com.hexagonal.to_do.domain.models.AdditionalTaksInfo;
+import com.hexagonal.to_do.domain.ports.out.ExternalServicePort;
+import com.hexagonal.to_do.infrastructure.adapters.ExternalServiceAdapter;
+import com.hexagonal.to_do.infrastructure.adapters.JpaTaskRepositoryAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.hexagonal.to_do.domain.ports.in.*;
+import com.hexagonal.to_do.domain.ports.out.TaskRepositoryPort;
+import com.hexagonal.to_do.application.services.TaskService;
+
+
+@Configuration
 public class ApplicationConfig {
+
+    @Bean
+    public TaskService taskService(TaskRepositoryPort taskRepositoryPort, GetAdditionalTaskInfoUseCase getAdditionalTaskInfoUseCase) {
+        return new TaskService(
+                new CreateTaskUseCaseImpl(taskRepositoryPort),
+                new DeleteTaskUseCaseImpl(taskRepositoryPort),
+                new RetrieveTaskUseCaseImpl(taskRepositoryPort),
+                new UpdateTaskUseCaseImpl(taskRepositoryPort),
+                getAdditionalTaskInfoUseCase
+                );
+    }
+
+    @Bean
+    public TaskRepositoryPort taskRepositoryPort(JpaTaskRepositoryAdapter jpaTaskRepositoryAdapter) {
+        return jpaTaskRepositoryAdapter;
+    }
+
+    @Bean
+    public GetAdditionalTaskInfoUseCase getAdditionalTaskInfoUseCase(ExternalServicePort externalServicePort) {
+        return new GetAdditionalTaskInfoUseCaseImpl(externalServicePort);
+    }
+
+//    @Bean
+//    public ExternalServicePort externalServicePort() {
+//        return new ExternalServiceAdapter();
+//    }
 }
