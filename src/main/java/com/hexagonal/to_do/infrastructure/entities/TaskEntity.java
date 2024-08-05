18acf5d4c848b1092dd720121843ba7708edbbca
valrichter <valenrichter11@gmail.com>
2024-08-05 16:33:42 -0300

    Add TaskEntity in infrastructure layer

--- src/main/java/com/hexagonal/to_do/infrastructure/entities/TaskEntity.java
+++ src/main/java/com/hexagonal/to_do/infrastructure/entities/TaskEntity.java
@@ -1,4 +1,83 @@
 package com.hexagonal.to_do.infrastructure.entities;
 
+import com.hexagonal.to_do.domain.models.Task;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+// Posible reemplazar por un Mapper
+@Entity
 public class TaskEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime createdAt;
+    private boolean completed;
+
+    public TaskEntity() {
+    }
+
+    public TaskEntity(Long id, String title, String description, LocalDateTime createdAt, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.completed = completed;
+    }
+
+    public static TaskEntity fromDomainModel(Task task) {
+        return new TaskEntity(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.getCreatedAt(),
+                task.isCompleted()
+        );
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
