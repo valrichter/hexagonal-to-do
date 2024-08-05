877f38d65d4c32949ffa7c7465dd724f0b1d878d
valrichter <valenrichter11@gmail.com>
2024-08-05 17:39:41 -0300

    Fix name

--- src/main/java/com/hexagonal/to_do/domain/models/AdditionalTaskInfo.java
+++ src/main/java/com/hexagonal/to_do/domain/models/AdditionalTaskInfo.java
@@ -1,12 +1,12 @@
 package com.hexagonal.to_do.domain.models;
 
-public class AdditionalTaksInfo {
+public class AdditionalTaskInfo {
 
     private final Long userId;
     private final String userName;
     private final String userEmail;
 
-    public AdditionalTaksInfo(Long userId, String userName, String userEmail) {
+    public AdditionalTaskInfo(Long userId, String userName, String userEmail) {
         this.userId = userId;
         this.userName = userName;
         this.userEmail = userEmail;
